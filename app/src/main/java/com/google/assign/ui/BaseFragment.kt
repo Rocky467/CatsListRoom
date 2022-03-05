c230f194616f66df028171fae1d3e4604162ed09
Rocky467 <chaudharirakesh467@gmail.com>
2022-03-05 16:36:43 +0530

    opt

--- app/src/main/java/com/google/assign/ui/BaseFragment.kt
+++ app/src/main/java/com/google/assign/ui/BaseFragment.kt
@@ -95,8 +95,7 @@ abstract class BaseFragment : Fragment(), CoroutineScope {
     }
 
     fun View.showError(message: String) {
-        Snackbar
-            .make(this, "Replace with your own action", Snackbar.LENGTH_LONG)
+        Snackbar.make(this, "Replace with your own action", Snackbar.LENGTH_LONG)
             .setAction("Action", null).show()
     }
 
